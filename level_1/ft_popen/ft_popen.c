a645245d3da23fce3c4d6efe8d34d29a58deee8d
alfa48 <948manuel@gmail.com>
2025-09-20 07:10:55 +0100

    ft_popen e 03 de picoshell

--- level_1/ft_popen/ft_popen.c
+++ level_1/ft_popen/ft_popen.c
@@ -1,89 +1,115 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
-/*
-int ft_popen(const char *file, char *const argv[], char type)
-{
-    if (!file || !argv || (type != 'r' && type != 'w'))
-        return -1;
-
-    int fd[2];
-    if (pipe(fd) < 0)
-        return -1;
-
-    pid_t pid = fork();
-    if (pid < 0)
-    {
-        close(fd[0]);
-        close(fd[1]);
-        return -1;
-    }
-
-    if (pid == 0)
-    {
-        if (type == 'r')
-        {
-            close(fd[0]);
-            if (dup2(fd[1], STDOUT_FILENO) < 0)
-                exit(-1);
-        }
-        else
-        {
-            close(fd[1]);
-            if (dup2(fd[0], STDIN_FILENO) < 0)
-                exit(-1);
-        }
-        close(fd[0]);
-        close(fd[1]);
-        execvp(file, (char *const *)argv);
-        exit(-1);
-    }
-
-    if (type == 'r')
-    {
-        close(fd[1]);
-        return fd[0];
-    }
-    close(fd[0]);
-    return fd[1];
-}
-*/
 
 int ft_popen(const char *file, char *const argv[], char type){
-    if (!file || !argv || (type != 'r' && type != 'w'))
-        return (-1);
-    int pipes[2];
-    if (pipe(pipes) < 0)
-        return (-1);
-    pid_t pid = fork();
-    if (pid < 0)
-        return (-1);
-    if (pid == 0){//filho
-        if (type == 'r'){
-            close(pipes[0]);
-            if (dup2(pipes[1], 1) < 0)
-                exit (-1);
-        }
-        else{
-            close(pipes[1]);
-            if (dup2(pipes[0], 0) < 0)
-                exit (-1);
-        }
-        execvp(file, argv);
-        exit (-1);
-    }
-    //pai
-    if (type == 'r'){
-        close(pipes[1]);
-        return (pipes[0]);
-    }
-        close(pipes[0]);
-        return (pipes[1]);
+	int pipes[2];
+	pid_t pid;
+	int status;
+	
+	if (!file || !argv || !argv[0]|| (type != 'r' && type != 'w'))
+		return (-1);
+	if (pipe(pipes) < 0)
+		return (-1);
+	pid = fork();
+	if (pid < 0)
+		return (-1);
+	if (pid == 0){// filho
+		if (type == 'r'){
+			if (dup2(pipes[1], 1) < 0){
+				close(pipes[0]);
+				close(pipes[1]);			
+				exit (-1);
+			}
+			close(pipes[0]);
+			close(pipes[1]);
+		}else{
+			if (dup2(pipes[0], 0) < 0){
+				close(pipes[0]);
+				close(pipes[1]);			
+				exit (-1);
+			}
+			close(pipes[0]);
+			close(pipes[1]);
+		}
+		execvp (file, argv);
+		exit (-1);
+	}
+	//pai
+	//wait(&status);	
+	if (type == 'r'){
+		close(pipes[1]);
+		return (pipes[0]);
+		
+	}
+	close(pipes[0]);
+	return (pipes[1]);
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*escrever*/
+
+
+int main(void)
+{
+    int fd = ft_popen("cat", (char *const[]){"cat", "-e", NULL}, 'w');
+    if (fd < 0) {
+        perror("ft_popen");
+        return 1;
+    }
+
+    write(fd, "hello world\n", 12);
+
+    close(fd);
+    return 0;
+}
+
+
+
+/*ler*/
+/*
 int main(int argc, char *argv[], char *envp[])
 {
     int fd = ft_popen("ls", (char *const[]){"ls", NULL}, 'r');
@@ -92,3 +118,4 @@ int main(int argc, char *argv[], char *envp[])
     while (read(fd, &ch, 1) > 0)
         write(0, &ch, 1);
 }
+*/
